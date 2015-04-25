b2098f7b73377cb7a9eacf3bd249bbe0ba346c5f
NolanSyKinsley <NolanSyKinsley@gmail.com>
2015-04-25 00:14:57 -0700

    Reduce piping

--- IconGrabber.sh
+++ IconGrabber.sh
@@ -5,8 +5,8 @@ ICONS=~/.local/share/icons/hicolor/32x32/apps/
 
 
 for file in "$STEAMAPPS"/*.acf; do 
-    ID=$(grep '"appid"' "$file" | head -1 | sed -r 's/[^"]*"appid"[^"]*"([^"]*)"/\1/') 
-    NAME=$(grep '"name"' "$file" | head -1 | sed -r 's/[^"]*"name"[^"]*"([^"]*)"/\1/') 
+    ID=$(grep -w -m 1 '"appid"' "$file" | sed -r 's/[^"]*"appid"[^"]*"([^"]*)"/\1/') 
+    NAME=$(grep -w -m 1 '"name"' "$file" | sed -r 's/[^"]*"name"[^"]*"([^"]*)"/\1/') 
 if [ ! -f $ICONS/steam_icon_$ID.png ]; then 
 	echo "$NAME's icon is missing, retrieving from web"
 	ICONHASH=$(steamcmd  +app_info_print $ID +exit | grep -w -m 1 "icon" | sed -r 's/[^"]*"icon"[^"]*"([^"]*)"/\1/') 
