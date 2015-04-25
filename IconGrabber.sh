bbe7135756749ccc6275bf5baec9a19f90bea9a0
NolanSyKinsley <NolanSyKinsley@gmail.com>
2015-04-24 23:58:41 -0700

    Cleanup of bad practices

--- IconGrabber.sh
+++ IconGrabber.sh
@@ -4,9 +4,9 @@ STEAMAPPS=~/.steam/steam/steamapps/
 ICONS=~/.local/share/icons/hicolor/32x32/apps/ 
 
 
-for file in $(ls $STEAMAPPS/*.acf -1v); do 
-    ID=$(cat "$file" | grep '"appid"' | head -1 | sed -r 's/[^"]*"appid"[^"]*"([^"]*)"/\1/') 
-    NAME=$(cat "$file" | grep '"name"' | head -1 | sed -r 's/[^"]*"name"[^"]*"([^"]*)"/\1/') 
+for file in "$STEAMAPPS"/*.acf; do 
+    ID=$(grep '"appid"' "$file" | head -1 | sed -r 's/[^"]*"appid"[^"]*"([^"]*)"/\1/') 
+    NAME=$(grep '"name"' "$file" | head -1 | sed -r 's/[^"]*"name"[^"]*"([^"]*)"/\1/') 
     ICONHASH=$(steamcmd  +app_info_print $ID +exit | grep -w -m 1 "icon" | sed -r 's/[^"]*"icon"[^"]*"([^"]*)"/\1/') 
 if [ ! -f $ICONS/steam_icon_$ID.png ]; then 
 	wget http://media.steampowered.com/steamcommunity/public/images/apps/$ID/$ICONHASH.jpg -O $ICONS/steam_icon_$ID.png
